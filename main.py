b34e7df72e39cf4ec25a6242b521c9760db47041
XKorp <103704390+XKorp00@users.noreply.github.com>
2022-11-22 20:44:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ def onmessage(update,bot:ObigramClient):
         #tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = ''
+        tl_admin_user = 'barberGeminis98'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -485,7 +485,7 @@ def onmessage(update,bot:ObigramClient):
   
 def main():
     #set in debug
-    bot_token = ''
+    bot_token = '5763989477:AAFGfvbYBhvW3-8_2Pe0qHL4z-sgkiySWN0'
     
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
